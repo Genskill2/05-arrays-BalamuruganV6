c9e539ec9c1f83c8f536393c05519174c4dae9d4
BalamuruganV6 <84074716+BalamuruganV6@users.noreply.github.com>
2021-06-14 20:23:29 +0530

    average

--- test_average.c
+++ test_average.c
@@ -12,3 +12,14 @@ int main(void) {
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
   }
+
+float average(int a[], int n)
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum=+a[i];
+    }
+    float avg=(float)sum/n;
+    return avg;
+}
