ad9bd3675aae69345a44e60fa2deef77989c5e72
BalamuruganV6 <84074716+BalamuruganV6@users.noreply.github.com>
2021-06-19 00:35:57 +0530

    Factors

--- test_factors.c
+++ test_factors.c
@@ -21,3 +21,39 @@ int main(void) {
   assert (ret[1] == 13);
   printf("Factors: passed\n");
 }
+
+int factors(int n , int ret[])
+{
+    int f[n],c1=0,c2=0;
+    for( int i=2; i<=n; i++)
+    {
+        int c=0;
+        for( int j=2; j<=i; j++)
+        {
+            if( i%j == 0)
+            {
+                c++;
+            }
+        }
+        if( c == 1)
+        {
+            f[c1]=i;
+            c1++;
+        }
+    }
+    for( int i=0; i<c1; i++)
+    {
+        for(int j=0;j<c1;j++)
+        {
+            if( n%f[i] == 0)
+            {
+                n = n/f[i];
+                ret[c2] = f[i];
+                c2++;
+            }
+            else
+                j = c1;
+        }
+    }
+    return c2;
+}
